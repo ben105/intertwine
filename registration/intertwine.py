7e566de1053d4c9bff3dd15ac90fde2b5ec94fb0
Ben Rooke <ben_rooke@icloud.com>
2015-01-02 15:56:31 +0000

    Adding a psuedo function for getting and posting friend requests

--- registration/intertwine.py
+++ registration/intertwine.py
@@ -84,51 +84,21 @@ def add_user():
 	#cur.connection.close()
 	return json.dumps( {"success":"true"} )
 
-@app.route('/api/v1/adduser/first/<first>/last/<last>/email/<email>/password/<password>')
-def email_register(first, last, email, password):
 
-	errors = dict()
-	err = check_name(first)
-	if err:
-		errors['first'] = err
-
-	err = check_name(last)
-	if err:
-		errors['last'] = err
-
-	err = check_password(password)
-	if err:
-		errors['password'] = err
-
-	if not validate_email(email, check_mx=True):
-		errors['email'] = 'Please enter a valid email address'
-
-	try:	
-		conn = psycopg2.connect("dbname=intertwine host=localhost user=brooke password=intertwine")
-	except Exception as exc:
-		print "Couldn't connect to postgres {}".format(exc)
-
-	cur = conn.cursor()
-	cur.connection.autocommit = True
-	try:
-		cur.execute("SELECT * FROM accounts WHERE email=%s", (email,))
-		rows = cur.fetchall()
-		if len(rows):
-			errors['email'] = 'An account already exists'
-	except Exception as exc:
-		print "Couldn't run execution"
-
-	
-	if len(errors.keys()):
-		return json.dumps(errors)		
-
-
-	try:
-		cur.execute("INSERT INTO accounts (email, u_id, first, last) VALUES (%s, %s, %s, %s);", (email, email, first, last))
-	except Exception as exc:
-		return json.dumps( { "PSQL access error":str(exc) } )
-	
-	return json.dumps( {"success":"true"} )
+@app.route('/api/v1/friendrequest', methods=['POST', 'GET'])
+def friend_request():
+	"""Friend request can either be a get
+	or a post. A get will simply return
+	the results of all pending friend 
+	requests, while a post will initiate
+	a friend request.
+	"""
+	if request.method == "GET":
+		user_id = request.form.get('user_id')
+		users_requests = friend_requests.get_requests(user_id)
+	elif request.method == "POST":
+		requester_id = request.form.get('requester_id')
+		requestee_id = request.form.get('requestee_id')
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0')
