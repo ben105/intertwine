c595322f23344d453f37cd00c9023334071c72fb
Ben Rooke <ben_rooke@filemaker.com>
2015-01-09 12:10:22 -0800

    Rae's commit

--- friends/friend_requests.py
+++ friends/friend_requests.py
@@ -1,12 +1,53 @@
-"""
-SELECT
-	accounts.first,
-	accounts.last,
-	accounts.facebook_id,
-	accounts.email
-FROM 
-	accounts, friends
-WHERE
-	friends.requester = accounts.id and
-	friends.requestee = %s
-"""
+# Pending Requests
+#
+# This query will retirive the list of friends from whom
+# you have a pending request.
+# It will ignore requests you've previously denied.
+def get_pending_requests():
+	pending_requests_query = """
+	SELECT
+		accounts.first,
+		accounts.last,
+		accounts.facebook_id,
+		accounts.email
+	FROM 
+		accounts, friend_requests
+	WHERE
+		friend_requests.denied = false and
+		friend_requests.requester = accounts.id and
+		friend_requests.requestee = %s
+	"""
+	# Run the query
+
+# Friend suggestions
+#
+# This query, as of this writing (01/07/15), will use
+# this list of Facebook friends to find accounts in 
+# the Intertwine database for friend suggestions.
+# Of course, the list of facebook friends is different
+# for each user.
+def get_friend_suggestions(user_id, friends_list):
+	# Generate a list of compairson conditions, with placeholders
+	conditions = list()
+	for i in range(0, len(friends_list)):
+			conditions.append("accounds.id = %s")
+	# Instantiate the friend suggestion query
+	# without the condition that it should match
+	# with the list of friends provided.
+	friend_suggestions = """
+	SELECT
+		accounts.first,
+		accounts.last,
+		accounts.facebook_id,
+		accounts.email
+	FROM 
+		accounts, friends
+	WHERE 
+		  accounts.id not in (SELECT blocked_account_id FROM blocks) and
+		  accounts.id not in (SELECT requestee_id FROM friend_requests) and
+		  accounts.id not in (SELECT requester_id FROM friend_requests)
+	"""
+	# Expand the conditions list into a condition string
+	condition_string = " or ".join(conditions)
+	friend_suggestions = friend_suggestions + " and (" + condition_string + ");"
+	# Run the query
