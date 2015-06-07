b36c270ec067b2be0b6d1cbd73fe3bf9edb6ae4f
Ben Rooke <ben_rooke@icloud.com>
2015-06-07 19:23:10 +0000

    Removed for loop, since the cp command can take multiple arguments and copy them all.

--- install.sh
+++ install.sh
@@ -1,17 +1,16 @@
 modules="accounts \
-	 activity \
-	 events \
-	 friends \
-	 registration \
-	 search \
-	 push.py"
-status intertwine > /dev/null | grep "running" && stop intertwine
+activity \
+events \
+friends \
+registration \
+search \
+push.py"
+status intertwine 2> /dev/null | grep "running" && stop intertwine
 mkdir /usr/lib/python2.7/intertwine 2> /dev/null
 touch /usr/lib/python2.7/intertwine/__init__.py
-for module in "$modules"
-do
-   cp -R "$module" /usr/lib/python2.7/intertwine/
-done
+
+cp -R $modules /usr/lib/python2.7/intertwine/
+
 mkdir -p /opt/intertwine/certs 2> /dev/null
 cp newck.pem /opt/intertwine/certs/
 cp intertwine /usr/bin/
